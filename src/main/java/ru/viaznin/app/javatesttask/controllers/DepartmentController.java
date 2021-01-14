e6afb758d835e0984844abb39b5dacce53aa57e4
Ilya Viaznin <viaznin123@gmail.com>
2021-01-15 01:20:02 +0500

    The department tree display is now normal, added simple editing.

--- src/main/java/ru/viaznin/app/javatesttask/controllers/DepartmentController.java
+++ src/main/java/ru/viaznin/app/javatesttask/controllers/DepartmentController.java
@@ -3,10 +3,11 @@ package ru.viaznin.app.javatesttask.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.viaznin.app.javatesttask.models.Department;
 import ru.viaznin.app.javatesttask.repositories.DepartmentsRepository;
 
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 /**
@@ -22,15 +23,35 @@ public class DepartmentController {
         this.departmentsRepository = departmentsRepository;
     }
 
-    @GetMapping("/get")
-    public String get(Model model) {
-        var departments = departmentsRepository.findAll()
+    @GetMapping
+    public String index(@RequestParam(required = false) Integer selectedDepartmentId, Model model) {
+        var departments = departmentsRepository.findAll();
+
+        var rootDepartments = departments
                 .stream()
                 .filter(d -> d.getParentDepartment() == null)
+                .sorted(Comparator.comparingLong(Department::getId))
                 .collect(Collectors.toList());
 
-        model.addAttribute("departments", departments);
+        model.addAttribute("rootDepartments", rootDepartments);
 
-        return "department/departmentsTree";
+        if (selectedDepartmentId != null) {
+            var selectedDepartment = departments
+                    .stream()
+                    .filter(d -> d.getId() == selectedDepartmentId)
+                    .findFirst()
+                    .orElseThrow();
+
+            model.addAttribute("selectedDepartment", selectedDepartment);
+        }
+
+        return "department/index";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String edit(@ModelAttribute("selectedDepartment") Department department, @PathVariable long id) {
+        departmentsRepository.fullDepartmentUpdate(department, id);
+
+        return "redirect:/department?selectedDepartmentId=" + id;
     }
 }
--- src/main/java/ru/viaznin/app/javatesttask/repositories/DepartmentsRepository.java
+++ src/main/java/ru/viaznin/app/javatesttask/repositories/DepartmentsRepository.java
@@ -7,4 +7,14 @@ import ru.viaznin.app.javatesttask.models.Department;
  * @author Ilya Viaznin
  */
 public interface DepartmentsRepository extends JpaRepository<Department, Long> {
+    default void fullDepartmentUpdate(Department department, long id){
+        var updatingDepartment = findById(id).orElse(null);
+
+        if(updatingDepartment == null)
+            return;
+
+        updatingDepartment.setName(department.getName());
+
+        save(updatingDepartment);
+    }
 }
